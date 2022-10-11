1fcde2b1edd69034009c4545e0fb00d2915a17ff
MaximGromak <maximgromak@gmail.com>
2022-10-12 01:22:53 +1000

    add db

--- src/main/java/ru/gromax/mybookshopapp/controllers/MainPageController.java
+++ src/main/java/ru/gromax/mybookshopapp/controllers/MainPageController.java
@@ -1,15 +1,25 @@
 package ru.gromax.mybookshopapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.gromax.mybookshopapp.data.BookService;
 
 @Controller
 @RequestMapping("/bookshop")
 public class MainPageController {
+    private final BookService bookService;
+
+    @Autowired
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @GetMapping("/main")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        model.addAttribute("bookData", this.bookService.getBookData());
         return "index";
     }
 }
