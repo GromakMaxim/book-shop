3f199c159478770f45ad3b9dceddc3cddaf0222a
MaximGromak <maximgromak@gmail.com>
2022-10-17 00:33:20 +1000

    standard dialect 1

--- src/main/java/ru/gromax/mybookshopapp/controllers/MainPageController.java
+++ src/main/java/ru/gromax/mybookshopapp/controllers/MainPageController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.gromax.mybookshopapp.data.BookService;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
 @RequestMapping("/bookshop")
 public class MainPageController {
@@ -21,6 +24,8 @@ public class MainPageController {
     public String mainPage(Model model) {
         model.addAttribute("bookData", this.bookService.getBookData());
         model.addAttribute("searchPlaceholder", "new search placeholder");
+        model.addAttribute("serverTime", new SimpleDateFormat("hh:mm:ss").format(new Date()));
+        model.addAttribute("placeholderTextPart2", "SERVER");
         return "index";
     }
 }
