0cd922c837fa14914fb4d33342c0a9e397d8b1e9
MaximGromak <maximgromak@gmail.com>
2022-11-01 02:00:37 +1000

    api-1

--- src/main/java/ru/gromax/mybookshopapp/data/book/BookCRUDRepository.java
+++ src/main/java/ru/gromax/mybookshopapp/data/book/BookCRUDRepository.java
@@ -1,7 +1,21 @@
 package ru.gromax.mybookshopapp.data.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BookCRUDRepository extends JpaRepository<Book, Integer> {
+
+    List<Book> findBookByAuthorFirstNameContaining(String authorFirstName);
+    List<Book> findBooksByTitleContaining(String bookTitle);
+    List<Book> findBooksByPriceOldBetween(Integer min, Integer max);
+    List<Book> findBookByPriceOldIs(Integer price);
+
+    @Query("from Book where isBestseller=1")
+    List<Book> getBestsellers();
+
+    @Query(value="SELECT * FROM books WHERE discount = (SELECT MAX(discount) FROM books)", nativeQuery = true)
+    List<Book> getBooksWithMaxDiscount();
 
 }
--- src/main/java/ru/gromax/mybookshopapp/data/book/BookService.java
+++ src/main/java/ru/gromax/mybookshopapp/data/book/BookService.java
@@ -18,4 +18,28 @@ public class BookService {
     public List<Book> getBookData() {
         return bookCRUDRepository.findAll();
     }
+
+    public List<Book> getBooksByAuthor(String authorName){
+        return bookCRUDRepository.findBookByAuthorFirstNameContaining(authorName);
+    }
+
+    public List<Book> getBooksByTitle(String title){
+        return bookCRUDRepository.findBooksByTitleContaining(title);
+    }
+
+    public List<Book> getBooksWithPriceBetween(Integer min, Integer max){
+        return bookCRUDRepository.findBooksByPriceOldBetween(min, max);
+    }
+
+    public List<Book> getBooksWithPrice(Integer price){
+        return bookCRUDRepository.findBookByPriceOldIs(price);
+    }
+
+    public List<Book> getBooksWithMaxPrice(){
+        return bookCRUDRepository.getBooksWithMaxDiscount();
+    }
+
+    public List<Book> getBestsellers(){
+        return bookCRUDRepository.getBestsellers();
+    }
 }
